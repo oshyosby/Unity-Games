6e485be1c5f69342b07e5885d9e82e3c8a47d163
Oscar <oscar.shanagher@gmail.com>
2024-07-18 09:15:07 +0100

    Remove Old Dev

--- Development/Fight Manager/Assets/Scripts/DataManager.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using System.Collections.Generic;
-
-public class DataManager {
-    public List<Record> records = new List<Record>();
-    public List<Person> people = new List<Person>();
-}
